e1f8feb50b34af4ca2becf02cfea2d6c7d50607b
Code83 <gonzalo.guajardodiaz@outlook.com>
2021-01-20 22:19:53 -0300

    actualiza validacion de rut

--- postgres-python/test-bd.py
+++ postgres-python/test-bd.py
@@ -4,15 +4,22 @@ conexion = psycopg2.connect(database="postgres",user="postgres",password="admin"
 cursor1 = conexion.cursor()
 
 sql="insert into usuarios (nombre, apellidopat, apellidomat, rut) values (%s,%s,%s,%s)"
+sql2="select * from usuarios where rut = %s"
 
 nombre = input("Ingresa tu nombre: ")
 apepat = input("Ingresa tu apellido paterno: ")
 apemat = input("ingresa tu apellido materno: ")
 rut = input("ingresa tu rut: ")
 
-
 datos = (nombre,apepat,apemat,rut)
-cursor1.execute(sql,datos)
+valida = (rut)
+
+if rut == '':
+    print("No ingresaste un rut")
+else:
+    
+#cursor1.execute(sql2,valida)
+ cursor1.execute(sql,datos)
 
 conexion.commit()
 conexion.close()
