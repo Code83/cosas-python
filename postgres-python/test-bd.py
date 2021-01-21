bedcde33f77818e9f367a8e1a84d3b4f773061eb
Code83 <gonzalo.guajardodiaz@outlook.com>
2021-01-21 00:16:22 -0300

    se agrega opcion para eliminar usuarios por rut

--- postgres-python/test-bd.py
+++ postgres-python/test-bd.py
@@ -4,22 +4,34 @@ conexion = psycopg2.connect(database="postgres",user="postgres",password="admin"
 cursor1 = conexion.cursor()
 
 sql="insert into usuarios (nombre, apellidopat, apellidomat, rut) values (%s,%s,%s,%s)"
-sql2="select * from usuarios where rut = %s"
+sql2="select * from usuarios where rut = '%s'"
+delete = "delete from usuarios where rut = %s"
 
-nombre = input("Ingresa tu nombre: ")
-apepat = input("Ingresa tu apellido paterno: ")
-apemat = input("ingresa tu apellido materno: ")
-rut = input("ingresa tu rut: ")
+menu = int(input("Ingresa una opcion 1.-Ingresar datos 2.-Borrar datos: "))
 
-datos = (nombre,apepat,apemat,rut)
-valida = (rut)
+if menu == 1:
+    nombre = input("Ingresa tu nombre: ")
+    apepat = input("Ingresa tu apellido paterno: ")
+    apemat = input("ingresa tu apellido materno: ")
+    rut = input("ingresa tu rut: ")
 
-if rut == '':
-    print("No ingresaste un rut")
-else:
+    datos = (nombre,apepat,apemat,rut)
     
-#cursor1.execute(sql2,valida)
- cursor1.execute(sql,datos)
+    if rut == '':
+        print("rut obligatorio, no se inserta registro")
+    else:
+        cursor1.execute(sql,datos)
 
-conexion.commit()
-conexion.close()
+ #cursor1.execute(delete,valida) línea para permitir borrar datos por rut
+
+        conexion.commit()
+        conexion.close()
+        print("Ingreso de datos OK")
+else:
+  rut = input("Ingresa el rut del usuario a eliminar: ")
+  valida = rut
+  cursor1.execute(delete,valida) #línea para permitir borrar datos por rut
+  
+  conexion.commit()
+  conexion.close()
+  print("Usuario eliminado")
